4161ad84a3f71ab9eb3b72b8f192d485fcdfd435
bryan <bryan.s.stebbing@outlook.com>
2023-09-27 21:07:44 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    int myNum = 1989;
+    String month = "July";
+    boolean isTodayAWeekend = false;
+    System.out.println(myNum);
+    System.out.println(month);
+    System.out.println(isTodayAWeekend);
+  }
+}
