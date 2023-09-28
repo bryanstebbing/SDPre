38ee6336b1e64171da76e4ff089e49d393d82374
bryan <bryan.s.stebbing@outlook.com>
2023-09-28 15:30:29 -0600

    complete Objective7Lab1.java

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,15 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+
+	do {
+  	  System.out.println(counter);
+  	  counter = counter - 1;
+	} while (counter > 0);
+
+      counter = counter - 1; 
+    }
+  }
+}
