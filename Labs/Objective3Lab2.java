d7f924919a433a6fbbc03675529044dd93cc188f
bryan <bryan.s.stebbing@outlook.com>
2023-09-28 09:39:04 -0600

    compelte Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,7 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		int side1 = 10;
+		int side2 = 10;
+		System.out.println("The hypotenuse of a triangle with sides 10 and 10 is " + Math.sqrt((side1 * side1) * (side2 * side2));
+	}
+}
