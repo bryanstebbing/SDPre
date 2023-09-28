2ad4b80f2581643e959d64d6b7b5c3d6fe883149
bryan <bryan.s.stebbing@outlook.com>
2023-09-28 09:38:05 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    int num1 = 7*2;
+    int num2 = 21;
+    int num3 = 3;
+    int sum1 = num1 + num2 + num3;
+    System.out.println(sum1/3);
+  }
+}
