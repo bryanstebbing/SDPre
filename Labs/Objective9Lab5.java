3d76a5412a5b20b0c414a6c2ecd9bb96a9c73ba0
bryan <bryan.s.stebbing@outlook.com>
2023-09-28 22:16:59 -0600

    complete Objective9Lab1.java

---
+++ Labs/Objective9Lab5.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Objective9Lab5 {
+
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+    double num1, num2;
+    boolean keepGoing = true;
+    int choice;
+    double answer = 0.0;
+
+    System.out.println("Please give me a number.");
+    num1 = kb.nextDouble();
+
+    System.out.println("Please give me another number.");
+    num2 = kb.nextDouble();
+
+    while (keepGoing) {
+      printMenu();
+      System.out.print("Which would you like to do? ");
+      choice = kb.nextInt();
+
+      switch (choice) {
+        case 1:
+          System.out.println(num1 + " + " + num2 + " = " + findSum(num1, num2));
+          break;
+        case 2:
+          double average = findAverage(num1, num2);
+          System.out.println("The average of " + num1 + " and " + num2 + " is: " + average);
+          break;
+        case 3: 
+          double tax = calcTax(num1,num2);
+          System.out.println("The amount in tax to be collected from a purchase of " + num1 + " and " + num2 + " is:" + tax);
+        case 4:
+          System.out.println("Goodbye!");
+          keepGoing = false;
+          break;
+        default:
+          System.out.println("Invalid choice. Please select a valid option.");
+          break;
+      }
+    }
+    kb.close();
+  }
+
+  public static void printMenu() {
+    System.out.println();
+    System.out.println("========= MENU =========");
+    System.out.println("|                      |");
+    System.out.println("|   1. Add Numbers     |");
+    System.out.println("|   2. Find Average    |");
+    System.out.println("|   3. Calculate Tax   |");
+    System.out.println("|   4. Exit            |");
+    System.out.println("|                      |");
+    System.out.println("========================");
+    System.out.println();
+  }
+
+  public static double findSum(double x, double y) {
+    return x + y;
+  }
+
+  public static double findAverage(double x, double y) {
+    return (x + y) / 2.0;
+  }
+
+  public static double calcTax(double x, double y) {
+    double sum = x + y;
+    return (sum * 8.31) / 100.0;
+  }
+}
